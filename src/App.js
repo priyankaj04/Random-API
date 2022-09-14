d1daf95c13d5e0b9ae25c3a01b7186fd143eff58
Priyanka J <priyankaj2002o4@gmail.com>
2022-09-14 10:52:11 +0530

    changing quotes api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import Topbar from './groups/topbar/topbar';
+import Body from './groups/body/body';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Topbar />
+			<Body />
+		</div>
+	);
 }
 
 export default App;
